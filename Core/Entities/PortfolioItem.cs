cf49bac90d9c165fb2db3c8ba0f20b5654018b46
ahmed nafady <ahmed.nafady@gmail.com>
2021-07-28 00:18:57 +0200

    Almost Finished My Portfolio

--- Core/Entities/PortfolioItem.cs
+++ Core/Entities/PortfolioItem.cs
@@ -2,8 +2,11 @@
 {
     public class PortfolioItem : EntityBase
     {
+
         public string ProjectName { get; set; }
         public string Description { get; set; }
         public string ImageUrl { get; set; }
+
+
     }
 }
---
+++ Infrastructure/Migrations/20210726140317_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Owner",
+                keyColumn: "Id",
+                keyValue: new Guid("7e01a9b1-a4b9-4461-80ed-aac6e865a387"));
+
+            migrationBuilder.InsertData(
+                table: "Owner",
+                columns: new[] { "Id", "AddressId", "Avatar", "FullName", "Profile" },
+                values: new object[] { new Guid("58a5dafc-b468-4aae-b74a-28b2b257d84f"), null, "Avatar.jpg", "Ahmed Nafady", ".Net Core Developer" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Owner",
+                keyColumn: "Id",
+                keyValue: new Guid("58a5dafc-b468-4aae-b74a-28b2b257d84f"));
+
+            migrationBuilder.InsertData(
+                table: "Owner",
+                columns: new[] { "Id", "AddressId", "Avatar", "FullName", "Profile" },
+                values: new object[] { new Guid("7e01a9b1-a4b9-4461-80ed-aac6e865a387"), null, "Avatar.jpg", "Ahmed Nafady", ".Net Core Developer" });
+        }
+    }
+}
